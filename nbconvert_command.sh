90616efa1d1d69de7bb5c739c23f18b02a5865d2
Jerome Queißer <none>
2019-12-08 21:11:31 +0100

    added shell-script for converting; renamed html-file

--- nbconvert_command.sh
+++ nbconvert_command.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
 jupyter nbconvert index.ipynb --to slides --no-prompt --TagRemovePreprocessor.remove_input_tags={\"remove_input\"} --post serve --SlidesExporter.reveal_scroll=True --SlidesExporter.reveal_transition=none
+mv index.slides.html index.html
